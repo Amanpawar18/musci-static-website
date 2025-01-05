a154b9e95629c07806852f60ab7667c15f51adc3
Aman Pawar <37652073+Amanpawar18@users.noreply.github.com>
2025-01-05 20:36:35 +0530

    Add files via upload

---
+++ src/components/HeroSection.tsx
@@ -0,0 +1,36 @@
+import Link from "next/link";
+import { Spotlight } from "./ui/Spotlight";
+import { Button } from "./ui/moving-border";
+
+function HeroSection() {
+  return (
+    <div className="h-[40rem] w-full rounded-md flex items-center justify-center bg-black/[0.96] antialiased bg-grid-white/[0.02] relative overflow-hidden mx-auto md:py-0 py-10">
+      <Spotlight
+        className="-top-40 left-0 md:left-60 md:-top-20"
+        fill="white"
+      />
+      <div className=" p-4 max-w-7xl  mx-auto relative z-10  w-full pt-20 md:pt-0">
+        <h1 className="text-4xl md:text-7xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50">
+          Master the art of music
+        </h1>
+        <p className="mt-4 font-normal text-base md:text-lg text-neutral-300 max-w-lg mx-auto">
+          Dive into our comprehensice music courses and transform your musical
+          journey today. Whether you are a begineer or looking to refine your
+          skills, join us to unlock your true potential.
+        </p>
+        <div className="mt-4 text-center">
+          <Link href={"/courses"}>
+            <Button
+              borderRadius="1.75rem"
+              className="bg-white dark:bg-black text-black dark:text-white border-neutral-200 dark:border-slate-800"
+            >
+              Explore courses
+            </Button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default HeroSection;
